2099daf824683c181b28dfdd510889164741cd85
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2021-08-20 08:22:12 -0500

    Classwork

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    Anytime a method is to be called from button click, it MUST HAVE a specific type
+    of method signature. If you don't use the right signature, your button will Not be
+    allowed to call his method.
+
+    public void methodName(view v){
+    }
+     */
+
+    public void makeName(View v){
+        EditText firstnameET = findViewById(R.id.firstNameEditText);
+        EditText lastnameET = findViewById(R.id.lastNameEditText);
+
+        String firstName = firstNameET.getText().toString();
+        String lastName = lastNameET.getText().toString();
+
+        String fullName = firstName + " " + lastName;
+        TextView fullNameTextView = findViewById(R.id.completeNameTextView);
+        fullNameTextView.setText(fullName);
     }
 }
