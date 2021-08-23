a251b149fe8457d819a896c7c9e27c63de26cfb9
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2021-08-23 08:18:07 -0500

    Added code to send message to new screen

--- app/src/main/java/com/example/myfirstapp/DisplayWelcomeActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayWelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayWelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class DisplayWelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_welcome);
+
+        // Receive the intent (like going to the mailbox)
+        Intent intent = getIntent();
+
+        // Extract the data using the constant name we created as its label
+        // Since this constant lives in MainActivity.java, that is why we need
+        // to refer to it in this way
+        String welcomeName = intent.getStringExtra(MainActivity.EXTRA_NAME);
+
+        // Get a reference to view on screen we want to display text in
+        TextView displayTV = findViewById(R.id.textView);
+
+        // Set the text to the TextView on the screen
+        displayTV.setText(welcomeName);
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creating a constant to be used with the intent,
+    // It's name should include the package name,
+    // AND describe what is being sent
+
+    public static final String EXTRA_NAME = "com.example.myfirstapp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
     of method signature. If you don't use the right signature, your button will Not be
     allowed to call his method.
 
-    public void methodName(view v){
+    public void methodName(View v){
     }
      */
 
+    // This method is an example of taking text from an EditText and displaying
+    // it on the SAME SCREEN (ACTIVITY)
     public void makeName(View v){
         EditText firstNameET = findViewById(R.id.firstNameEditText);
         EditText lastNameET = findViewById(R.id.lastNameEditText);
@@ -35,4 +44,26 @@ public class MainActivity extends AppCompatActivity {
         TextView fullNameTextView = findViewById(R.id.completeNameTextView);
         fullNameTextView.setText(fullName);
     }
+
+    // This method is an example of taking text from an EditText on one screen
+    // and displaying it on A DIFFERENT SCREEN. We are using intents to switch
+    // activities
+    public void sendMessage(View v){
+        // Creating the ability to switch screens (addressing the envelope)
+        Intent intent = new Intent(this, DisplayWelcomeActivity.class);
+        EditText firstNameET = findViewById(R.id.firstNameEditText);
+        EditText lastNameET = findViewById(R.id.lastNameEditText);
+
+        // Getting all the info from the screen we want to send
+        String firstName = firstNameET.getText().toString();
+        String lastName = lastNameET.getText().toString();
+
+        String fullName = firstName + " " + lastName;
+        //Sending the info with the intent (filing the envelope)
+        intent.putExtra(EXTRA_NAME, fullName);
+        // Telling AS to start this activity which will switch the screens
+        // like mailing the envelope
+        startActivity(intent);
+    }
+
 }
